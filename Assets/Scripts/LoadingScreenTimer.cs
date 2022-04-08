eb5db6be53cc7fcde52fdf9097eee4e9e55cb6c0
MRVLS_Wheat <d.minchev.b98@gmail.com>
2022-04-08 10:43:16 +0200

    Revert "Added: RegisterScreen (frontend) - Email verificationScreen (frontend) - Forgot PasswordScreen (frontend) - WeatherForcastScreen (Both)"
    This reverts commit 1a98d0d05419d132d2192c4a0a64b600963883ba.

--- Assets/Scripts/LoadingScreenTimer.cs
+++ Assets/Scripts/LoadingScreenTimer.cs
@@ -4,34 +4,18 @@ using UnityEngine;
 
 public class LoadingScreenTimer : MonoBehaviour
 {
-    float loadingTimer = 5.0f;
-    bool loadingScreen = false;
+    public float loadingTimer;
     public GameObject loadScreen;
     public GameObject fieldSelectScreen;
 
     // Update is called once per frame
     void Update()
     {
-        if (loadingScreen == true)
-        {
-            LoadingStart();
-        }
-    }
-
-    public void LoadingScreen()
-    {
-        loadingScreen = true;
-    }
-
-    void LoadingStart()
-    {
         loadingTimer -= Time.deltaTime;
         if (loadingTimer < 0)
         {
             loadScreen.SetActive(false);
             fieldSelectScreen.SetActive(true);
-            loadingScreen = false;
-            loadingTimer = Random.Range(1.0f, 5.0f);
         }
     }
 }
